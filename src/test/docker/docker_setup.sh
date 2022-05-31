0f2bc6f8a89052c928db27b9e4b9a1735daceb36
zoleoho <oleksandr.ohorodnik@tietoevry.com>
2022-05-31 16:12:15 +0300

    Add dockerized local setup for gc-conductor plugin
    Add configuration to build Gerrit image, that includes gc-executor and
    gc-conductor plugin
    Add configuration to build PostgreSQL image
    Add docker-compose.yaml to run Gerrit and PostgreSQL images build,
    containers creation and start
    Add shell script docker_setup.sh to wrap docker-compose build and up
    commands and pass them command line arguments
    
    Change-Id: Iec6ec686726060b43d1b088bd115fbdc7039cc3f

---
+++ src/test/docker/docker_setup.sh
@@ -0,0 +1,73 @@
+#!/bin/bash -e
+
+if [ $# -eq 0 ]; then
+  echo "please add arguments --gc-executor-path and --postgres-driver-path"
+else
+  while [ $# -ne 0 ]; do
+    case "$1" in
+    "--help")
+      echo "Usage: sh $0 [--option $value]"
+      echo
+      echo "[--gc-conductor-path]            Path to gc-conductor.jar"
+      echo
+      echo "[--gc-executor-path]            Path to gc-executor.jar"
+      echo
+      echo "[--postgres-driver-path]        Path to postgresql-42.2.5.jar"
+      echo
+      echo "[--postgres-image-path]         PostgreSQL image path without image name (optional)"
+      echo
+      echo "[--gerrit-image-path]           Gerrit image path without image name (optional)"
+      echo
+      echo "[--detached-mode]               Argument to enable detached mode, [-d]"
+      exit 0
+      ;;
+    "--gc-conductor-path")
+          export GC_CONDUCTOR_PATH=$2
+          shift
+          shift
+          ;;
+    "--gc-executor-path")
+      export GC_EXECUTOR_PATH=$2
+      shift
+      shift
+      ;;
+    "--postgres-driver-path")
+      export POSTGRES_DRIVER_PATH=$2
+      shift
+      shift
+      ;;
+    "--postgres-image-path")
+      export POSTGRES_IMAGE_PATH=$2
+      shift
+      shift
+      ;;
+    "--gerrit-image-path")
+      export GERRIT_IMAGE_PATH=$2
+      shift
+      shift
+      ;;
+    "--detached-mode")
+      export DETACHED_MODE=$2
+      shift
+      shift
+      ;;
+    *)
+      echo "Unknown option argument: $1"
+      shift
+      shift
+      ;;
+    esac
+  done
+
+  if [ -z "$POSTGRES_IMAGE_PATH" ]; then
+    export POSTGRES_IMAGE_PATH=armdocker.rnd.ericsson.se/dockerhub-ericsson-remote
+  fi
+  if [ -z "$GERRIT_IMAGE_PATH" ]; then
+    export GERRIT_IMAGE_PATH=armdocker.rnd.ericsson.se/dockerhub-ericsson-remote/gerritcodereview
+  fi
+
+  docker-compose build --build-arg POSTGRES_DRIVER=$POSTGRES_DRIVER_PATH \
+    --build-arg POSTGRES_IMAGE=$POSTGRES_IMAGE_PATH \
+    --build-arg GERRIT_IMAGE=$GERRIT_IMAGE_PATH
+  docker-compose up $DETACHED_MODE
+fi
