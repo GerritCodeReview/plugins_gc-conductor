5fc4121e449d4456e72bcafe0235ee4772022462
zoleoho <oleksandr.ohorodnik@tietoevry.com>
2022-05-31 16:12:15 +0300

    Add dockerized local setup for the gc-conductor plugin
    Add configuration to build Gerrit image, that includes gc-executor and
    gc-conductor plugin.
    Add configuration to build PostgreSQL image.
    Add docker-compose.yaml to run Gerrit and PostgreSQL images build,
    containers creation and start.
    Add shell script docker_setup.sh to wrap docker-compose build and up
    commands and initialize build parameters from command line arguments.
    
    Feature: Issue 13098
    Change-Id: I1073c8e67bdbbe449734672be8b9a0dd72ae5d4e

---
+++ src/test/docker/gerrit/start_gerrit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+cp /var/gerrit/etc/gerrit.config.orig /var/gerrit/etc/gerrit.config
+cp /var/gerrit/etc/gc.config.orig /var/gerrit/etc/gc.config
+cp /var/gerrit/etc/log4j2.xml.orig /var/gerrit/etc/log4j2.xml
+
+echo "Initializing Gerrit site ..."
+java -jar /var/gerrit/bin/gerrit.war init -d /var/gerrit --batch
+
+touch /var/gerrit/logs/{gc_log,error_log,httpd_log,sshd_log,replication_log} && tail -f /var/gerrit/logs/* | grep --line-buffered -v 'HEAD /' &
+
+echo "Running gc-executor ..."
+java -DconfigFile=/var/gerrit/etc/gc.config -jar /var/gerrit/plugins/gc-executor.jar --console-log &
+
+echo "Running Gerrit ..."
+java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 /var/gerrit/bin/gerrit.war daemon -d /var/gerrit
