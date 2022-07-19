9dd9a242a6e022f63cc351e4d598ab5114aa462b
zoleoho <oleksandr.ohorodnik@tietoevry.com>
2022-07-19 16:44:10 +0300

    Introduce dockerized local setup small fixes
    Remove physical drives in docker containers to reduce
    disk space utilization;
    Add re-indexind during Gerrit start.
    
    Change-Id: I27a2b46dd93ee9c1778877bf9d03cfcc2cd2f13e

--- src/test/docker/gerrit/start_gerrit.sh
+++ src/test/docker/gerrit/start_gerrit.sh
@@ -4,8 +4,14 @@ cp /var/gerrit/etc/gerrit.config.orig /var/gerrit/etc/gerrit.config
 cp /var/gerrit/etc/gc.config.orig /var/gerrit/etc/gc.config
 cp /var/gerrit/etc/log4j2.xml.orig /var/gerrit/etc/log4j2.xml
 
-echo "Initializing Gerrit site ..."
-java -jar /var/gerrit/bin/gerrit.war init -d /var/gerrit --batch
+if [[ ! -f /var/gerrit/etc/ssh_host_ed25519_key ]]
+then
+  echo "Initializing Gerrit site ..."
+  java -jar /var/gerrit/bin/gerrit.war init -d /var/gerrit --batch
+fi
+
+echo "Reindexing Gerrit ..."
+java -jar /var/gerrit/bin/gerrit.war reindex -d /var/gerrit
 
 touch /var/gerrit/logs/{gc_log,error_log,httpd_log,sshd_log,replication_log} && tail -f /var/gerrit/logs/* | grep --line-buffered -v 'HEAD /' &
 
